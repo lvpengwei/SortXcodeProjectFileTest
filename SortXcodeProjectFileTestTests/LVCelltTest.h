1fbbb98053c2671ae917db7b5c5b4bed6fd29785
yangxudong <123191047@qq.com>
2014-04-19 20:51:08 +0800

    添加测试文件

---
+++ SortXcodeProjectFileTestTests/LVCelltTest.h
@@ -0,0 +1,13 @@
+//
+//  LVCelltTest.h
+//  SortXcodeProjectFileTest
+//
+//  Created by dd on 14-4-19.
+//  Copyright (c) 2014年 com.lvpw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LVCelltTest : UITableViewCell
+
+@end
---
+++ SortXcodeProjectFileTestTests/LVCelltTest.m
@@ -0,0 +1,29 @@
+//
+//  LVCelltTest.m
+//  SortXcodeProjectFileTest
+//
+//  Created by dd on 14-4-19.
+//  Copyright (c) 2014年 com.lvpw. All rights reserved.
+//
+
+#import "LVCelltTest.h"
+
+@implementation LVCelltTest
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
